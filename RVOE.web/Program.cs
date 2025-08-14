5d283313571aae17c5b9cb0c2352d678f8ff0525
Eduardo Zenteno <zenten0hern4ndez@gmail.com>
2025-08-14 10:08:33 -0600

    Funcionamiento correcto del login sin hashing de contraseñas (puedo logearme y está funcionando correctamente el proyecto)

--- RVOE.web/Program.cs
+++ RVOE.web/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<PladseLocalContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("PladseLocalContext")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PladseLocal")));
 
 builder.Services.AddSession();
 
